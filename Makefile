15cee70e59be1eeb173765502d05558773e65747
SergeiAP <Sergei.A.P@yandex.com>
2022-01-29 00:58:46 +0300

    RENAME some entities

--- Makefile
+++ Makefile
@@ -1,15 +1,15 @@
-launch_db:
+launch_docker_compose_db:
 	chmod +x init_docker_named_volumes.sh
 	./init_docker_named_volumes.sh
 	docker-compose up -d
-rebuild_restart_db:
-	docker stop postgresql-autodiller-db-test
-	docker rm -f postgresql-autodiller-db-test
-	docker rmi -f postgresql-autodiller-db-test
+rebuild_restart_docker_compose_db:
+	docker stop autodiller-db_postgres
+	docker rm -f autodiller-db_postgres
+	docker rmi -f autodiller-db_postgres
 	docker-compose up -d
 init_insert_db:
-	docker exec -it postgresql-autodiller-db-test bash
+	docker exec -it autodiller-db_postgres bash
 	psql -U postgres -d autodiller_salon -f /root/sql_dmls/init_insert.sql
 simulate_operations_db:
-	docker exec -it postgresql-autodiller-db-test bash
+	docker exec -it autodiller-db_postgres bash
 	psql -U postgres -d autodiller_salon -f /root/sql_dmls/simulate_operations.sql
--- init_docker_named_volumes.sh
+++ init_docker_named_volumes.sh
@@ -1,8 +1,8 @@
 #Create a volume to store data from database
-docker volume create postgresql-autodiller-test-db-data 
-sudo chmod +007 $(docker volume inspect --format '{{ .Mountpoint }}' postgresql-autodiller-test-db-data)
+docker volume create autodiller-db-data 
+sudo chmod +007 $(docker volume inspect --format '{{ .Mountpoint }}' autodiller-db-data)
 
 #Create a volume to store some useful sql's e.g. for data inserting to use them in the container on-demand
-docker volume create  postgresql-autodiller-test-db-sqls
-sudo chmod +007 $(docker volume inspect --format '{{ .Mountpoint }}' postgresql-autodiller-test-db-sqls)
-cp ./dml/* $(docker volume inspect --format '{{ .Mountpoint }}' postgresql-autodiller-test-db-sqls)
+docker volume create  autodiller-db-sqls
+sudo chmod +007 $(docker volume inspect --format '{{ .Mountpoint }}' autodiller-db-sqls)
+cp ./dml/* $(docker volume inspect --format '{{ .Mountpoint }}' autodiller-db-sqls)
